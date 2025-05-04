1d12458c7e06b885b8e2b9cfa48db81d04e55875
Raji Chithrangathan <raji.chithrangathan@goldenscent.com>
2025-05-04 13:53:22 +0400

    initial commit

---
+++ src/test/java/com/raji/todo/ToDoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raji.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
