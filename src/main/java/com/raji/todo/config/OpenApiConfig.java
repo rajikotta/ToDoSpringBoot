16dce94c183b7126c026cf70428040a69bd67fb4
Raji Chithrangathan <raji.chithrangathan@goldenscent.com>
2025-05-07 13:43:18 +0400

    warning fix

---
+++ src/main/java/com/raji/todo/config/OpenApiConfig.java
@@ -0,0 +1,9 @@
+package com.raji.todo.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "https://todo.ractechhub.com")})
+
+public class OpenApiConfig {
+}
