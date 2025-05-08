7972727b43a1305450065c5021e3dfcb54f40342
Raji Chithrangathan <raji.chithrangathan@goldenscent.com>
2025-05-08 12:01:46 +0400

    testing

--- src/main/java/com/raji/todo/WelcomeController.java
+++ src/main/java/com/raji/todo/WelcomeController.java
@@ -1,9 +1,11 @@
 package com.raji.todo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+    @GetMapping("welcome")
     public String hello(){
         return "Hello World";
     }
